d1cf82dc01dad119afc397568c287b6fc5856ffc
Bui Gábor <bui.gabor97@gmail.com>
2020-12-10 19:15:50 +0100

    Refactored app to use async/await

--- public/js/index.js
+++ public/js/index.js
@@ -26,7 +26,10 @@ const clearBtn = document.querySelector('.create-lesson-clear');
 let isAuthReady = false;
 
 // event listener setup
-formElement.addEventListener('submit', addLesson);
+formElement.addEventListener('submit', function (e) {
+	e.preventDefault();
+	addLesson();
+});
 modalLessonClose.addEventListener('click', handleCloseLessonModal);
 clearBtn.addEventListener('click', handleClear);
 overlay.addEventListener('click', handleCloseLessonModal);
@@ -70,18 +73,19 @@ function handleCloseLessonModal() {
 	overlay.classList.remove('dark');
 }
 
-function init(user) {
-	quill.root.focus();
-	renderLessons()
-		.then(() => {
-			appContainer.removeAttribute('hidden');
-			profileElement.removeAttribute('hidden');
-			modal.setAttribute('hidden', '');
-			preAuthContainer.setAttribute('hidden', '');
-			overlay.setAttribute('hidden', '');
-			avatarElement.setAttribute('src', user.photoURL);
-		})
-		.catch(console.log);
+async function init(user) {
+	try {
+		quill.root.focus();
+		await renderLessons();
+		appContainer.removeAttribute('hidden');
+		profileElement.removeAttribute('hidden');
+		modal.setAttribute('hidden', '');
+		preAuthContainer.setAttribute('hidden', '');
+		overlay.setAttribute('hidden', '');
+		avatarElement.setAttribute('src', user.photoURL);
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 function handleEditClick(lesson) {
@@ -129,201 +133,196 @@ function handleViewClick(lesson) {
 	overlay.classList.add('dark');
 }
 
-function renderLessons() {
+async function renderLessons() {
 	handleClear();
 	// render lesson cards
-	return db
+	const snapshot = await db
 		.collection('users')
 		.doc(firebase.auth().currentUser.uid)
-		.get()
-		.then((snapshot) => {
-			if (lessonsContainer.childElementCount) {
-				lessonsContainer.innerHTML = '';
-			}
+		.get();
+	if (lessonsContainer.childElementCount) {
+		lessonsContainer.innerHTML = '';
+	}
 
-			if (snapshot.data() && snapshot.data().lessons.length) {
-				snapshot.data().lessons.forEach(({ title, content, id }) => {
-					const lessonCard = document.createElement('div');
-					lessonCard.addEventListener('click', lessonHandler);
-					lessonCard.classList.add('lesson-card');
-					lessonCard.setAttribute('data-id', id);
+	if (snapshot.data() && snapshot.data().lessons.length) {
+		snapshot.data().lessons.forEach(({ title, content, id }) => {
+			const lessonCard = document.createElement('div');
+			lessonCard.addEventListener('click', lessonHandler);
+			lessonCard.classList.add('lesson-card');
+			lessonCard.setAttribute('data-id', id);
 
-					const buttonContainer = document.createElement('div');
-					buttonContainer.classList.add('lesson-card-content-buttons');
+			const buttonContainer = document.createElement('div');
+			buttonContainer.classList.add('lesson-card-content-buttons');
 
-					const titleContainer = document.createElement('div');
-					titleContainer.classList.add('lesson-card-title-container');
+			const titleContainer = document.createElement('div');
+			titleContainer.classList.add('lesson-card-title-container');
 
-					const lessonTitle = document.createElement('h2');
-					lessonTitle.classList.add('lesson-card-title');
-					lessonTitle.textContent = title;
+			const lessonTitle = document.createElement('h2');
+			lessonTitle.classList.add('lesson-card-title');
+			lessonTitle.textContent = title;
 
-					const lessonContent = document.createElement('div');
-					lessonContent.classList.add(
-						'lesson-card-content',
-						'ql-editor',
-						'ql-container',
-					);
-					lessonContent.innerHTML = content;
+			const lessonContent = document.createElement('div');
+			lessonContent.classList.add(
+				'lesson-card-content',
+				'ql-editor',
+				'ql-container',
+			);
+			lessonContent.innerHTML = content;
 
-					const lessonRemoveBtn = document.createElement('button');
-					lessonRemoveBtn.classList.add('button');
-					lessonRemoveBtn.id = 'delete';
+			const lessonRemoveBtn = document.createElement('button');
+			lessonRemoveBtn.classList.add('button');
+			lessonRemoveBtn.id = 'delete';
 
-					const removeIcon = document.createElement('img');
-					removeIcon.setAttribute('alt', 'remove lesson icon');
-					removeIcon.setAttribute('src', './images/cancel-white.svg');
-					removeIcon.id = 'delete';
+			const removeIcon = document.createElement('img');
+			removeIcon.setAttribute('alt', 'remove lesson icon');
+			removeIcon.setAttribute('src', './images/cancel-white.svg');
+			removeIcon.id = 'delete';
 
-					const editIcon = document.createElement('img');
-					editIcon.setAttribute('alt', 'edit lesson icon');
-					editIcon.setAttribute('src', './images/edit-white.svg');
-					editIcon.id = 'edit';
+			const editIcon = document.createElement('img');
+			editIcon.setAttribute('alt', 'edit lesson icon');
+			editIcon.setAttribute('src', './images/edit-white.svg');
+			editIcon.id = 'edit';
 
-					const lessonEditBtn = document.createElement('button');
-					lessonEditBtn.classList.add('button');
-					lessonEditBtn.id = 'edit';
+			const lessonEditBtn = document.createElement('button');
+			lessonEditBtn.classList.add('button');
+			lessonEditBtn.id = 'edit';
 
-					const lessonViewBtn = document.createElement('button');
-					lessonViewBtn.textContent = 'VIEW LESSON';
-					lessonViewBtn.id = 'view';
-					lessonViewBtn.classList.add('button');
+			const lessonViewBtn = document.createElement('button');
+			lessonViewBtn.textContent = 'VIEW LESSON';
+			lessonViewBtn.id = 'view';
+			lessonViewBtn.classList.add('button');
 
-					titleContainer.appendChild(lessonTitle);
-					lessonRemoveBtn.appendChild(removeIcon);
-					titleContainer.appendChild(lessonRemoveBtn);
-					lessonCard.appendChild(titleContainer);
-					lessonCard.appendChild(lessonContent);
-					buttonContainer.appendChild(lessonViewBtn);
-					lessonEditBtn.appendChild(editIcon);
-					buttonContainer.appendChild(lessonEditBtn);
-					lessonCard.appendChild(buttonContainer);
-					lessonsContainer.appendChild(lessonCard);
-				});
-			} else {
-				const noLessons = document.createElement('p');
-				noLessons.classList.add('no-lessons');
-
-				noLessons.textContent = 'No lessons :(';
-				lessonsContainer.appendChild(noLessons);
-			}
+			titleContainer.appendChild(lessonTitle);
+			lessonRemoveBtn.appendChild(removeIcon);
+			titleContainer.appendChild(lessonRemoveBtn);
+			lessonCard.appendChild(titleContainer);
+			lessonCard.appendChild(lessonContent);
+			buttonContainer.appendChild(lessonViewBtn);
+			lessonEditBtn.appendChild(editIcon);
+			buttonContainer.appendChild(lessonEditBtn);
+			lessonCard.appendChild(buttonContainer);
+			lessonsContainer.appendChild(lessonCard);
 		});
+	} else {
+		const noLessons = document.createElement('p');
+		noLessons.classList.add('no-lessons');
+
+		noLessons.textContent = 'No lessons :(';
+		lessonsContainer.appendChild(noLessons);
+	}
 }
 
-function addUser(user) {
+async function addUser(user) {
 	const { uid } = user;
+	const snapshot = await db.collection('users').where('uid', '==', uid).get();
+	const userExists = !snapshot.empty;
 
-	db.collection('users')
-		.where('uid', '==', uid)
-		.get()
-		.then((snapshot) => {
-			const userExists = !snapshot.empty;
-
-			if (userExists) {
-				console.log('user already exists');
-			} else {
-				console.log('user does not exist. Adding...');
-				db.collection('users')
-					.doc(uid)
-					.set({ uid: uid, lessons: [] })
-					.catch((error) => {
-						console.error('Error adding document: ', error);
-					});
-			}
-		});
+	if (userExists) {
+		console.log('user already exists');
+	} else {
+		console.log('user does not exist. Adding...');
+		db.collection('users')
+			.doc(uid)
+			.set({ uid: uid, lessons: [] })
+			.catch((error) => {
+				console.error('Error adding document: ', error);
+			});
+	}
 }
 
-function addLesson(e) {
-	e.preventDefault();
-
+async function addLesson() {
 	const content = quill.root.innerHTML;
 	const isEditView = createLessonContainer
 		.getAttribute('view')
 		.includes('edit-lesson');
 
 	if (isEditView) {
-		const id = createLessonContainer.getAttribute('view').split(':')[1];
+		try {
+			const id = createLessonContainer.getAttribute('view').split(':')[1];
 
-		db.collection('users')
-			.doc(firebase.auth().currentUser.uid)
-			.get()
-			.then((snapshot) => {
-				const newLessons = snapshot.data().lessons.map((lesson) => {
-					if (lesson.id === id) {
-						lesson.title = lessonInput.value;
-						lesson.content = content;
-					}
-					return lesson;
+			const snapshot = await db
+				.collection('users')
+				.doc(firebase.auth().currentUser.uid)
+				.get();
+
+			const newLessons = snapshot.data().lessons.map((lesson) => {
+				if (lesson.id === id) {
+					lesson.title = lessonInput.value;
+					lesson.content = content;
+				}
+				return lesson;
+			});
+
+			await snapshot.ref.update({ lessons: newLessons });
+			renderLessons();
+		} catch (error) {
+			console.log(error);
+		}
+	} else {
+		try {
+			await db
+				.collection('users')
+				.doc(firebase.auth().currentUser.uid)
+				.update({
+					lessons: firebase.firestore.FieldValue.arrayUnion({
+						title: lessonInput.value,
+						content,
+						id: String(Math.floor(Math.random() * 90000 + 10000)),
+					}),
 				});
 
-				snapshot.ref
-					.update({ lessons: newLessons })
-					.then(renderLessons)
-					.catch(console.log);
-			});
-	} else {
-		db.collection('users')
-			.doc(firebase.auth().currentUser.uid)
-			.update({
-				lessons: firebase.firestore.FieldValue.arrayUnion({
-					title: lessonInput.value,
-					content,
-					id: String(Math.floor(Math.random() * 90000 + 10000)),
-				}),
-			})
-			.then(() => {
-				lessonInput.value = '';
-				renderLessons();
-			})
-			.catch(console.log);
+			lessonInput.value = '';
+			renderLessons();
+		} catch (error) {
+			console.log(error);
+		}
 	}
 }
 
-function removeLesson(deleteId) {
-	db.collection('users')
-		.doc(firebase.auth().currentUser.uid)
-		.get()
-		.then((snapshot) => {
-			const newLessons = snapshot
-				.data()
-				.lessons.filter((lesson) => lesson.id !== deleteId);
+async function removeLesson(deleteId) {
+	try {
+		const snapshot = await db
+			.collection('users')
+			.doc(firebase.auth().currentUser.uid)
+			.get();
 
-			snapshot.ref
-				.update({ lessons: newLessons })
-				.then(renderLessons)
-				.catch(console.log);
-		});
+		const newLessons = snapshot
+			.data()
+			.lessons.filter((lesson) => lesson.id !== deleteId);
+
+		await snapshot.ref.update({ lessons: newLessons });
+		renderLessons();
+	} catch (error) {
+		console.log(error);
+	}
 }
 
-signInElement.addEventListener('click', () => {
-	firebase
-		.auth()
-		.signInWithPopup(provider)
-		.then(({ user }) => {
-			init(user);
-			addUser(user);
-		})
-		.catch((error) => {
-			const errorCode = error.code;
-			const errorMessage = error.message;
+signInElement.addEventListener('click', async () => {
+	try {
+		const { user } = await firebase.auth().signInWithPopup(provider);
 
-			console.error(`${errorCode} ${errorMessage}`);
-		});
+		init(user);
+		addUser(user);
+	} catch (error) {
+		const errorCode = error.code;
+		const errorMessage = error.message;
+
+		console.error(`${errorCode} ${errorMessage}`);
+	}
 });
 
-signOutElement.addEventListener('click', () => {
-	firebase
-		.auth()
-		.signOut()
-		.then(() => {
-			console.log('signed out success');
-			appContainer.setAttribute('hidden', '');
-			profileElement.setAttribute('hidden', '');
-			preAuthContainer.removeAttribute('hidden');
-			overlay.removeAttribute('hidden');
-			modal.removeAttribute('hidden');
+signOutElement.addEventListener('click', async () => {
+	try {
+		await firebase.auth().signOut();
+		console.log('signed out success');
+		appContainer.setAttribute('hidden', '');
+		profileElement.setAttribute('hidden', '');
+		preAuthContainer.removeAttribute('hidden');
+		overlay.removeAttribute('hidden');
+		modal.removeAttribute('hidden');
 
-			handleCloseLessonModal();
-		})
-		.catch(console.log);
+		handleCloseLessonModal();
+	} catch (error) {
+		console.log(error);
+	}
 });
